8d1317918faf51c486bbbf232ea9d3ea975adcf1
Oksana Zherevko <squirrelksi@ukr.net>
2021-02-12 21:17:05 +0200

    Add tasks 6-10.

---
+++ src/task-7.js
@@ -0,0 +1,14 @@
+// Стартовый код
+const fruits = ['яблоко', 'персик', 'груша', 'банан'];
+// Пиши код ниже этой строки
+const fruitsArrayLength = fruits.length;
+
+console.log(fruits.length);
+
+
+// Задание
+// Объяви переменную fruitsArrayLength и присвой ей длину массива fruits используя свойство length.
+
+// Тесты
+// Объявлена переменная fruitsArrayLength.
+// Значение переменной fruitsArrayLength это число 4.
