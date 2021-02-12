9b79f9ca4562ea7103953c8064eb7274c9bfc3c7
Oksana Zherevko <squirrelksi@ukr.net>
2021-02-12 20:01:58 +0200

    Add files.

---
+++ src/task-22.js
@@ -0,0 +1,23 @@
+function checkIfCanAccessContent(subType) {
+  const canAccessContent = subType === 'pro' || subType === 'vip'; // дополни эту строку
+
+  return canAccessContent;
+}
+
+console.log(checkIfCanAccessContent('pro'));
+console.log(checkIfCanAccessContent('starter'));
+console.log(checkIfCanAccessContent('vip'));
+console.log(checkIfCanAccessContent('free'));
+
+// Задание
+// Функция checkIfCanAccessContent(subType) проверяет, может ли пользователь получить доступ к контенту. Проверка происходит по типу подписки. Получить доступ могут только пользователи с подпиской pro или vip.
+
+// Присвой переменной canAccessContent выражение проверки подписки. Если значение параметра subType равно строкам 'pro' или 'vip', пользователь получит доступ. Результатом выражения проверки будет буль true или false.
+
+// Тесты
+// Объявлена функция checkIfCanAccessContent(subType).
+// В выражении проверки использован оператор ||.
+// Вызов checkIfCanAccessContent('pro') возвращает true.
+// Вызов checkIfCanAccessContent('starter') возвращает false.
+// Вызов checkIfCanAccessContent('vip') возвращает true.
+// Вызов checkIfCanAccessContent('free') возвращает false.
